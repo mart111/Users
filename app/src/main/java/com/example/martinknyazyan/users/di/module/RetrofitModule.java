db4624acc9fbf15f6d31762c610af3196f96635c
Martin Knyazyan <mart34111@gmail.com>
2018-08-07 18:43:17 +0400

    Call adapter factory implemented

--- app/src/main/java/com/example/martinknyazyan/users/di/module/RetrofitModule.java
+++ app/src/main/java/com/example/martinknyazyan/users/di/module/RetrofitModule.java
@@ -5,6 +5,7 @@ import dagger.Provides;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
@@ -16,6 +17,7 @@ public class RetrofitModule {
         return new Retrofit.Builder()
                 .baseUrl("http://api.randomuser.me")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(client)
                 .build();
     }
